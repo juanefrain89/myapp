57743396a596807be930ceb9e1f6e0279798b4a7
juanefrain89 <163329535+juanefrain89@users.noreply.github.com>
2024-04-10 15:19:30 -0600

    Update index.js

--- noseq/index.js
+++ noseq/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 app.use(cors({
-  origin:"https://ultimo-5mz4.onrender.com"
+  origin:"https://dentapp.vercel.app/"
 }));
 
 
