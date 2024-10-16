1ecfe1850d7c0c941e1dac2589679d26270b37d9
juanefrain89 <302420141@ute.edu.mx>
2024-10-16 17:16:47 -0600

    cambio

--- noseq/index.js
+++ noseq/index.js
@@ -6,22 +6,22 @@ const multer = require("multer");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
-
+const jwt = require('jsonwebtoken');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.use(cors({
   origin: "https://omar-7ebn.onrender.com"
 }));
 
-// Configuración de multer para almacenamiento de imágenes
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./imagenes"); // Carpeta donde se guardarán las imágenes
+    cb(null, "./imagenes"); 
   },
   filename: (req, file, cb) => {
-    const ext = file.originalname.split(".").pop(); // Obtiene la extensión del archivo
-    cb(null, `${Date.now()}.${ext}`); // Asigna un nombre único basado en la fecha
+    const ext = file.originalname.split(".").pop();
+    cb(null, `${Date.now()}.${ext}`); 
   }
 });
 
@@ -42,12 +42,11 @@ const dbConfig = {
 };
 
 
+
 app.use(mysqlConexion(mysql, dbConfig, "single"));
 
-// Ruta para servir imágenes estáticamente
-app.use('/imagenes', express.static(path.join(__dirname, 'imagenes')));
 
-// Ruta para obtener todas las patrullas
+app.use('/imagenes', express.static(path.join(__dirname, 'imagenes')));
 app.get("/", (req, res) => {
   const query = `SELECT * FROM patrullas`;
   
@@ -68,11 +67,9 @@ app.get("/", (req, res) => {
           console.log("No se encontraron resultados.");
           return res.status(404).send("No se encontraron resultados.");
         } else {
-          
-          const resultsWithUrls = result.map(item => ({
+             const resultsWithUrls = result.map(item => ({
             ...item,
-           
-            imagen: item.imagen ? `https://ddcd-5.onrender.com/imagenes/${item.imagen}` : null // Agregar la URL de la imagen
+             imagen: item.imagen ? `https://ddcd-5.onrender.com/imagenes/${item.imagen}` : null 
           }));
           res.status(200).send(resultsWithUrls);
         }
@@ -84,7 +81,7 @@ app.get("/", (req, res) => {
   }
 });
 
-// Ruta para insertar datos en la base de datos
+
 app.post("/l", upload.single('imagen'), (req, res) => {
   if (!req.file) {
     return res.status(400).send('No se ha recibido ninguna imagen.');
@@ -92,7 +89,7 @@ app.post("/l", upload.single('imagen'), (req, res) => {
   const { placa, ubicacion, contacto, unidad, referencias, latitud, longitud } = req.body;
   const imagenNombre = req.file ? req.file.filename : null; 
 
-  const sql = 'INSERT INTO patrullas (placa, ubicacion, contacto, unidad, referencias, imagen, latitud, longitud) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+  const sql = 'INSERT INTO patrullas_pendientes (placa, ubicacion, contacto, unidad, referencias, imagen, latitud, longitud) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
   const values = [placa, ubicacion, contacto, unidad, referencias, imagenNombre, latitud, longitud];
 
   req.getConnection((err, con) => {
@@ -122,9 +119,10 @@ app.post("/l", upload.single('imagen'), (req, res) => {
 
 
 app.post("/registro", (req, res) => {
+ console.log(req.body);
  
-  const { correo, password, rol } = req.body;
-  const imagenNombre = req.file ? req.file.filename : null; 
+  const { correo, password, rol } = req.body;  
+console.log(correo, password, rol);
 
   const sql = 'INSERT INTO usuarios (correo, contraseña, rol) VALUES (?, ?, ?)';
   const values = [correo, password, rol];
@@ -147,7 +145,44 @@ app.post("/registro", (req, res) => {
 });
 
 
+app.post("/login", (req, res) => {
 
+  const {correo, password} = req.body
+  console.log(req.body);
+  
+  const query = `SELECT * FROM usuarios WHERE correo = '${correo}' and contraseña ='${password}' `;
+  
+  try {
+    req.getConnection((err, con) => {
+      if (err) {
+        console.error("Error al conectar a la base de datos:", err);
+        return res.status(500).send("Error al conectar a la base de datos");
+      }
+
+      con.query(query, (err, result) => {
+        if (err) {
+          console.error("Error al ejecutar la consulta:", err);
+          return res.status(500).send("Error al ejecutar la consulta en la base de datos");
+        }
+        
+        if (result.length === 0) {
+          console.log("No se encontraron resultados.");
+          return res.status(404).send("No se encontraron resultados.");
+        } else {
+          const id = result[0].id;
+          const rol = result[0].rol;
+          const token = jwt.sign({ correo: correo }, "el botas", {expiresIn:"10m"});
+          console.log(id);
+          res.status(200).json({token, id,rol});
+          console.log("correcto");
+        }
+      });
+    });
+  } catch (error) {
+    console.error("Error inesperado:", error);
+    res.status(500).send("Error inesperado");
+  }
+});
 
 
 
